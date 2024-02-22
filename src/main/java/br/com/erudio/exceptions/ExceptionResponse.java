43fbadd3d16a6cc29cebb7b9c06e32a2d4102838
gloooooory <glo8ory@gmail.com>
2024-02-22 15:58:49 -0300

    Create ExceptionResponse.java

---
+++ src/main/java/br/com/erudio/exceptions/ExceptionResponse.java
@@ -0,0 +1,42 @@
+package br.com.erudio.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Date timeStamp;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date timeStamp, String message, String details) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
