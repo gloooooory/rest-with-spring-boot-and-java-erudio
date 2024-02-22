2e09682c020195c4c91451d061fb2d2ba5ccec41
gloooooory <glo8ory@gmail.com>
2024-02-22 15:58:53 -0300

    Create UnsupportedMathOperationException.java

---
+++ src/main/java/br/com/erudio/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,13 @@
+package br.com.erudio.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public UnsupportedMathOperationException(String message) {
+        super(message);
+    }
+}
