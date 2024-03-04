ac8779a9acdb6dc02c3d8977e677088d0e7a8c04
gloooooory <glo8ory@gmail.com>
2024-03-04 11:46:01 -0300

    feat: PersonVOV2 methods

--- src/test/java/br/com/erudio/person/MockPerson.java
+++ src/test/java/br/com/erudio/person/MockPerson.java
@@ -1,9 +1,11 @@
 package br.com.erudio.person;
 
 import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.model.Person;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MockPerson {
@@ -16,6 +18,11 @@ public class MockPerson {
     public static PersonVO mockVO() {
         return mockVO(0);
     }
+
+    public static PersonVOV2 mockVOV2() {
+        return mockVOV2(0);
+    }
+
 
     public static List<Person> mockEntityList() {
         List<Person> persons = new ArrayList<>();
@@ -50,6 +57,17 @@ public class MockPerson {
         person.setGender(((number % 2) == 0) ? "Male" : "Female");
         person.setKey(number.longValue());
         person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    public static PersonVOV2 mockVOV2(Integer number) {
+        PersonVOV2 person = new PersonVOV2();
+        person.setAddress("Address Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2) == 0) ? "Male" : "Female");
+        person.setKey(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        person.setBirthDay(new Date());
         return person;
     }
 
